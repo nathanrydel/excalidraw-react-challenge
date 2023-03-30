9df469250b99570585f83bc94af2b3135efbb739
Nathan Rydel <nathanrydel@gmail.com>
2023-03-30 09:32:45 -0700

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  root: true
+};
