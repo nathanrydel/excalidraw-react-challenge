e5be6259ed7e6f0079a9cd94dd6d49ad9dd1882e
Nathan Rydel <nathanrydel@gmail.com>
2023-03-30 12:56:08 -0700

    final content fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,43 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+'use client'
+import { useState } from 'react';
+import './page.css';
 
-const inter = Inter({ subsets: ['latin'] })
+export default function App() {
+  const [cells, setCells] = useState(['a', 'b', 'c']);
 
-export default function Home() {
+  function changeCellContent(newCellValue: string, indexToUpdate: number) {
+    setCells((prevCells) => 
+      prevCells.map((cell, idx) => (idx === indexToUpdate ? newCellValue : cell)))
+  }
+
+  function handlePlusClicked(idx: number) {
+    setCells((prevCells) => [
+      ...prevCells.slice(0, idx + 1),
+      '_',
+      ...prevCells.slice(idx + 1),
+    ])
+  }
+
+  const combineStrings = cells.join('');
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className='App'>
+      <section className='cells'>
+        {cells.map((cell, idx) => (
+          <div key={idx} className='cell'>
+            <input onChange={(e) => changeCellContent(e.currentTarget.value, idx)} value={cell}></input>
+            {idx < cells.length - 1 && (
+              <span className='plus' onClick={() => handlePlusClicked(idx)}>
+                &nbsp;
+              </span>
+            )}
+          </div>
+        ))}
+        
+      </section>
+      <section className='CombinedArea'>
+        <div className="output">{combineStrings}</div>
+      </section>
     </main>
-  )
+  );
 }
